a2c16c8e642e490eee71c81829dcd4037c1e9e8e
DiegoParra <diego16parra@hotmail.com>
2018-03-12 11:20:23 -0500

    Se agrego el proyecto builder

---
+++ Reporte/src/reporte/ReportDirector.java
@@ -0,0 +1,22 @@
+package reporte;
+/*The runtime object build the complex objects*/
+public class ReportDirector {
+	  private IReportBuilder myBuilder;
+
+	  public ReportDirector(IReportBuilder builder){
+	    myBuilder = builder;
+	  }
+
+	  public void buildReport(){
+	    // report building logic/steps
+	    myBuilder.buildReportTitle();
+	    myBuilder.buildHeader();
+	    myBuilder.buildPreface();
+	    myBuilder.buildContent();
+	    myBuilder.buildFooter();
+	  }
+
+	  public Report getReport(){
+	    return this.myBuilder.getReport();
+	  }
+}
