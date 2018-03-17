7914125f9dfebeacdc16e41e04e36577923dfdcb
Diego Parra <diego16parra@hotmail.com>
2018-03-17 14:18:50 -0500

    Agregue la ventana del patron iterator, faltan agregarle cambios a la ventana

--- Reporte/src/IU/VentanaPrincipal.java
+++ Reporte/src/IU/VentanaPrincipal.java
@@ -5,26 +5,188 @@
  */
 package IU;
 
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
  *
  * @author diego
  */
-public class VentanaPrincipal extends JFrame implements ActionListener{
-    private JPanel panel;
+public class VentanaPrincipal extends JFrame {
+public static final String nuevaLinea = "\n";
+  public static final String GENERAR_REPORTE = "Generar Reporte";
+  public static final String SALIR = "Salir";
+  public static final String ESTUDIANTES = "Estudiantes";
+  public static final String ASIGNATURAS = "Asignaturas";
+  
 
-    public VentanaPrincipal() {
-    }
-    
+  private JComboBox cmbTipoReporte;
+  private JPanel pSearchCriteria;
+  private JTextArea taReporte;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public VentanaPrincipal() throws Exception {
+    super("Iterator Pattern - Example");
+
+    // Create controls
+    cmbTipoReporte = new JComboBox();
+    taReporte = new JTextArea(15, 20);
+    //taSelectedCandidates.setMargin(new Insets(1,1,1,1));
+    taReporte.setEditable(false);
+
+    pSearchCriteria = new JPanel();
+
+    cmbTipoReporte.addItem(VentanaPrincipal.ESTUDIANTES);
+    cmbTipoReporte.addItem(VentanaPrincipal.ASIGNATURAS);
+   
+
+    //Create Labels
+    JLabel lblCertificationType =
+      new JLabel("Tipo de reporte:");
+    JLabel lblSelectedCandidates = new JLabel("Reporte:");
+
+    //Create the open button
+    JButton btnGetSelectedCandidates =
+      new JButton(VentanaPrincipal.GENERAR_REPORTE);
+   // btnGetSelectedCandidates.setMnemonic(VentanaPrincipal.VK_R);
+    JButton btnExit = new JButton(VentanaPrincipal.SALIR);
+    btnExit.setMnemonic(KeyEvent.VK_X);
+
+    buttonHandler vf = new buttonHandler(this);
+
+    btnGetSelectedCandidates.addActionListener(vf);
+    btnExit.addActionListener(vf);
+
+    //For layout purposes, put the buttons in a separate panel
+    JPanel buttonPanel = new JPanel();
+
+    JPanel panel = new JPanel();
+
+    GridBagLayout gridbag2 = new GridBagLayout();
+    panel.setLayout(gridbag2);
+    panel.add(btnGetSelectedCandidates);
+    panel.add(btnExit);
+
+    GridBagConstraints gbc2 = new GridBagConstraints();
+    gbc2.gridx = 0;
+    gbc2.gridy = 0;
+    gridbag2.setConstraints(btnGetSelectedCandidates, gbc2);
+    gbc2.gridx = 3;
+    gbc2.gridy = 0;
+    gridbag2.setConstraints(btnExit, gbc2);
+
+    //****************************************************
+
+    GridBagLayout gridbag = new GridBagLayout();
+    buttonPanel.setLayout(gridbag);
+    GridBagConstraints gbc = new GridBagConstraints();
+
+    buttonPanel.add(lblCertificationType);
+    buttonPanel.add(cmbTipoReporte);
+    buttonPanel.add(lblSelectedCandidates);
+    buttonPanel.add(taReporte);
+    buttonPanel.add(panel);
+
+    gbc.insets.top = 5;
+    gbc.insets.bottom = 5;
+    gbc.insets.left = 5;
+    gbc.insets.right = 5;
+
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gridbag.setConstraints(lblCertificationType, gbc);
+    gbc.anchor = GridBagConstraints.WEST;
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gridbag.setConstraints(cmbTipoReporte, gbc);
+    gbc.anchor = GridBagConstraints.WEST;
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gridbag.setConstraints(lblSelectedCandidates, gbc);
+
+    gbc.anchor = GridBagConstraints.WEST;
+    gbc.gridx = 1;
+    gbc.gridy = 1;
+    gridbag.setConstraints(taReporte, gbc);
+
+    gbc.insets.left = 2;
+    gbc.insets.right = 2;
+    gbc.insets.top = 40;
+    gbc.gridx = 1;
+    gbc.gridy = 6;
+    gridbag.setConstraints(panel, gbc);
+
+
+    //****************************************************
+    //Add the buttons and the log to the frame
+    Container contentPane = getContentPane();
+    contentPane.add(buttonPanel, BorderLayout.CENTER);
+    try {
+      UIManager.setLookAndFeel(new WindowsLookAndFeel());
+      SwingUtilities.updateComponentTreeUI(
+        VentanaPrincipal.this);
+    } catch (Exception ex) {
+      System.out.println(ex);
     }
-    
-    
+  }
+
+
+  public String getCertificationType() {
+    return (String) cmbTipoReporte.getSelectedItem();
+  }
+  public void setSelectedCandidates(String selectedCandidates) {
+    taReporte.setText(selectedCandidates);
+  }
+
 }
+class buttonHandler implements ActionListener {
+  VentanaPrincipal manager;
+  public void actionPerformed(ActionEvent e) {
+
+    if (e.getActionCommand().equals(VentanaPrincipal.SALIR)) {
+      System.exit(1);
+    }
+    if (e.getActionCommand().equals(VentanaPrincipal.GENERAR_REPORTE)) {
+      String selection = manager.getCertificationType();
+      //AllCandidates ac = new VentanaPrincipal();
+      /*
+      Iterator certCandidates =
+        ac.getCertifiedCandidates(selection);
+      String selectedCandidates =
+        "Name --- Cert Type --- Location" + "\n" + 
+          "--------------------------------------";
+
+      while (certCandidates.hasNext()) {
+        Candidate c = (Candidate) certCandidates.next();
+        selectedCandidates = selectedCandidates + "\n" +
+            c.getName() + " - " + c.getCertificationType() + 
+              " - " + c.getLocation();
+      }
+      manager.setSelectedCandidates(selectedCandidates);*/
+    }
+  }
+  public buttonHandler() {
+  }
+  public buttonHandler(VentanaPrincipal inmanager) {
+    manager = inmanager;
+  }
+
+}
+
+    
+  
--- Reporte/src/reporte/ReportTest.java
+++ Reporte/src/reporte/ReportTest.java
@@ -1,26 +1,48 @@
 package reporte;
+
+import IU.VentanaPrincipal;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
 /*App client*/
 public class ReportTest {
-	  public static void main(String[] args) {
-		    IReportBuilder builder = new  ReportBuilderSE();
-		    ReportDirector reportDirector = new ReportDirector(builder);
-		    reportDirector.buildReport();
-		    Report report=reportDirector.getReport();
-		    //use report object as per business
-		    System.out.println(report.getReportTitle());
-		    System.out.println(report.getHeader());
-		    System.out.println(report.getPreface());
-		    System.out.println(report.getContent());
-		    System.out.println(report.getFooter());
-		    IReportBuilder builder1 = new  ReportBuilderTEL();
-		    ReportDirector reportDirector1 = new ReportDirector(builder1);
-		    reportDirector1.buildReport();
-		    Report report1=reportDirector1.getReport();
-		    //use report object as per business
-		    System.out.println(report1.getReportTitle());
-		    System.out.println(report1.getHeader());
-		    System.out.println(report1.getPreface());
-		    System.out.println(report1.getContent());
-		    System.out.println(report1.getFooter());
-		  }
+
+    public static void main(String[] args) throws Exception {
+
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setVisible(true);
+        JFrame frame = new VentanaPrincipal();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        }
+        );
+
+        //frame.pack();
+        frame.setSize(600, 500);
+        frame.setVisible(true);
+
+        IReportBuilder builder = new ReportBuilderSE();
+        ReportDirector reportDirector = new ReportDirector(builder);
+        reportDirector.buildReport();
+        Report report = reportDirector.getReport();
+        //use report object as per business
+        System.out.println(report.getReportTitle());
+        System.out.println(report.getHeader());
+        System.out.println(report.getPreface());
+        System.out.println(report.getContent());
+        System.out.println(report.getFooter());
+        IReportBuilder builder1 = new ReportBuilderTEL();
+        ReportDirector reportDirector1 = new ReportDirector(builder1);
+        reportDirector1.buildReport();
+        Report report1 = reportDirector1.getReport();
+        //use report object as per business
+        System.out.println(report1.getReportTitle());
+        System.out.println(report1.getHeader());
+        System.out.println(report1.getPreface());
+        System.out.println(report1.getContent());
+        System.out.println(report1.getFooter());
+    }
 }
